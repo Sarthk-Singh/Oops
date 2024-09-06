21a413f2cf2fa94899d46afbe79a6cffa17d69cf
Sarthk-Singh <sarthaksinghddn@gmail.com>
2024-09-06 08:38:35 +0530

    new

---
+++ s4.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class hotel
+{
+    int Rno;
+    string name;
+    float amount;
+    int tariff;
+    int DOS;
+
+    public:
+
+    void checkIn(int Rno , string name , int tariff ,int DOS){
+      this-> Rno = Rno;
+      this-> name = name;
+      this-> tariff = tariff;
+      this-> DOS = DOS;
+    }
+     
+  
+
+    void checkout()
+    {
+        calc();
+        cout << "Room Number : " << Rno << endl;
+        cout << "Name : " << name << endl;
+        cout << "Tariff : " << tariff << endl;
+        cout << "DOS : " << DOS << endl;
+        cout << "Amount : " << amount << endl;
+    }
+
+    void calc()
+    {
+        amount = DOS*tariff;
+        if(amount > 10000) {
+           amount*=1.05;
+        }
+
+    }
+};
+
+int main()
+{
+    string name;
+    int room , tar , days;
+    int n;
+    cout<<"Enter the number of customers"<<endl;
+    cin>>n;
+    hotel op[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter details for Customer no."<<i+1<<endl;
+        cout<<"Enter your name: ";
+        cin >> name;
+        cout<<"Enter your room number: ";
+        cin>>room;
+        cout<<"Enter your tariff: ";
+        cin>>tar;
+        cout<<"Enter number of days: ";
+        cin>>days;
+        op[i].checkIn(room,name,tar,days);
+    }
+    cout<<"**CHECKOUT**"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        op[i].checkout();
+    }
+}
---
+++ s5.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+class bank
+{
+    string name;
+    int acNumber;
+    string acType;
+    float balance;
+    public:
+    bank()
+    {
+        name="";
+        acNumber=-1;
+        acType="";
+        balance=0;
+    }
+    void createAC(string name,int acNumber,string acType)
+    {
+        this->name=name;
+        this->acNumber=acNumber;
+        this->acType=acType;
+    } 
+    void show_details()
+    {
+        cout<<"Name: "<<name<<endl;
+        //cout<<"Acount Number: "<<acNumber<<endl;
+        //cout<<"Acount Type"<<acType<<endl;
+        cout<<"Balance: "<<balance<<endl;
+    }
+    int deposite(int dpAmount)
+    {
+        balance+=dpAmount;
+        cout<<"New balance: "<<balance<<endl;
+    }
+    void withdraw(int wdAmount)
+    {
+        if(wdAmount>balance)
+        {
+            cout<<"Low Bank Balance XD"<<endl;
+        }
+        else
+        {
+            balance-=wdAmount;
+            cout<<"New Balance: "<<endl;
+        }
+    }
+};
+int main()
+{
+    int n,in,dp,wd,Cno;
+    string name,acType;
+    int acNumber;
+    cout<<"Enter the number of accounts to create"<<endl;
+    cin>>n;
+    
+    bank ac[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter the details of customer "<<i+1<<endl;
+        cout<<"Enter the name of account holder"<<endl;
+        cin>>name;
+        cout<<"Enter the account number: "<<endl;
+        cin>>acNumber;
+        cout<<"Enter the account type: "<<endl;
+        cin>>acType;
+        ac[i].createAC(name,acNumber,acType);
+    }
+    for(int i=0;i<n;i++)
+    {
+        //cout<<"Enter customer number:"<<endl;
+        //cin>>Cno;
+        cout<<"PRESS '1' to deposite // Press '2' to withdraw // PRESS '0' to check balance"<<endl;
+        cin>>in;
+        switch(in)
+        {
+            case 0:
+                ac[i].show_details();
+                break;
+            case 1:
+                cout<<"Enter the amount you want to deposite: "<<endl;
+                cin>>dp;
+                ac[i].deposite(dp);
+                break;
+            case 2:
+                cout<<"Enter the amount to withdraw: "<<endl;
+                cin>>wd;
+                ac[i].withdraw(wd);
+                break;
+            default:
+                cout<<"Invalid input"<<endl;
+        }
+    }
+    return 0;
+}
